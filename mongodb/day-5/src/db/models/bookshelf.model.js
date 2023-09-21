732355aab21f2b9037a9cf66193f9778de20b642
zettacamp-akhmad-fahri <akhmad.fahri@zettacamp.pro>
2023-09-21 08:01:53 +0700

    setup dependencies

---
+++ mongodb/day-5/src/db/models/bookshelf.model.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose')
+
+const bookshelfSchema = mongoose.Schema({
+    position: {
+        column: {
+            type: String,
+            required: true
+        },
+        row: {
+            type: Number,
+            required: true
+        },
+    },
+
+    books: [{
+        type: mongoose.Types.ObjectId,
+        required: true,
+        ref: "Book"
+    }],
+
+    discount: [{
+        month: {
+            type: String,
+            required: true
+        },
+        value: {
+            type: Number,
+            required: true
+        }
+    }]
+})
+
+module.exports = mongoose.model('Bookshelf', bookshelfSchema)
